ba93212c330fc01b82d8c296116db906c763aca7
p.revington <p.revington@gmail.com>
2010-11-28 15:43:14 +0100

    Basic form update

--- src/rooms.js
+++ src/rooms.js
@@ -1,28 +1,87 @@
 ﻿(function($) {
-    $.fn.occform = function(options) {
+    $.fn.rooms = function(options) {
+
+
 
         var settings = {
-            'min-adults': 2,
-            'max-adults': 4,
-            'min-children': 0,
-            'max-children': 3,
-            'min-occupations': 1,
-            'max-occupations': 4,
-            'min-child-age': 0,
-            'max-child-age': 14,
+            'min_adults': 2,
+            'max_adults': 4,
+            'min_children': 0,
+            'max_children': 3,
+            'min_child_age': 0,
+            'max_child_age': 14,
             'serilize': function() { },
             'occupationAdd': function() { },
-            'occupationRemove': function() { }
+            'occupationRemove': function() { },
+            'default_room': ["aa", "aa"]
         };
 
-        function printForm() { return "<p>the form</p>"; };
+
+
+
 
         if (options) {
             $.extend(settings, options);
         }
 
+        function setRooms(rooms, r) {
+            if (!rooms) {
+                rooms = [];
+            }
+            if (r == rooms.length) {
+                return rooms;
+            } else if (r < rooms.length) {
+                return rooms.slice(0, r);
+            } else {
+                while (r != rooms.length) {
+                    rooms.push(settings.default_room);
+                }
+                return rooms;
+            }
+        }
+
+        function formhtml(rooms) {
+            return "<p>the form " + rooms.length + "</p>";
+        };
+
+
+        function getRandomNumber(range) {
+            return Math.floor(Math.random() * range);
+        }
+
+        function getRandomChar() {
+            var chars = "0123456789abcdefghijklmnopqurstuvwxyz";
+            return chars.substr(getRandomNumber(39), 1);
+        }
+
+        function randomID(size) {
+            var str = "";
+            for (var i = 0; i < size; i++) {
+                str += getRandomChar();
+            }
+            return str;
+        }
+
+
         return this.each(function(i, el) {
-            $(this).html(printForm());
+            var $this = $(this), data = {
+                rooms: setRooms([], $this.val()),
+                form_id: "pngr_rooms_" + randomID(10)
+            };
+
+            $this.data(data);
+
+            $(this).change(function() {
+
+                var current_data = $(this).data();
+                current_data.rooms = setRooms(current_data.rooms, $(this).val());
+                $(this).data(current_data);
+                $("#" + current_data.form_id).html(formhtml(current_data.rooms));
+
+            });
+
+            $this.after('<div id="' + data.form_id + '">' + formhtml(data.rooms) + '</div>');
+
         });
     };
 })(jQuery);
