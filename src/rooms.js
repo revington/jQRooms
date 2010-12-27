7aaaeb28d6d100e874199c77fe4b9ec87ac3d185
p.revington <p.revington@gmail.com>
2010-12-27 20:39:41 +0100

    It work's

--- src/rooms.js
+++ src/rooms.js
@@ -1,10 +1,8 @@
 ﻿(function($) {
     $.fn.rooms = function(options) {
-
-
-
+        
         var settings = {
-            'min_adults': 2,
+            'min_adults': 1,
             'max_adults': 4,
             'min_children': 0,
             'max_children': 3,
@@ -13,75 +11,185 @@
             'serilize': function() { },
             'occupationAdd': function() { },
             'occupationRemove': function() { },
-            'default_room': ["aa", "aa"]
-        };
-
-
-
-
-
-        if (options) {
-            $.extend(settings, options);
-        }
-
-        function setRooms(rooms, r) {
-            if (!rooms) {
-                rooms = [];
-            }
-            if (r == rooms.length) {
-                return rooms;
-            } else if (r < rooms.length) {
-                return rooms.slice(0, r);
-            } else {
-                while (r != rooms.length) {
-                    rooms.push(settings.default_room);
-                }
-                return rooms;
-            }
-        }
-
-        function formhtml(rooms) {
-            return "<p>the form " + rooms.length + "</p>";
-        };
-
-
-        function getRandomNumber(range) {
-            return Math.floor(Math.random() * range);
-        }
-
-        function getRandomChar() {
-            var chars = "0123456789abcdefghijklmnopqurstuvwxyz";
-            return chars.substr(getRandomNumber(39), 1);
-        }
-
-        function randomID(size) {
-            var str = "";
-            for (var i = 0; i < size; i++) {
-                str += getRandomChar();
-            }
-            return str;
-        }
-
-
-        return this.each(function(i, el) {
-            var $this = $(this), data = {
-                rooms: setRooms([], $this.val()),
-                form_id: "pngr_rooms_" + randomID(10)
+            'default_room': ["a", "a"],
+            'adults_label': 'adults',
+            'children_label': 'children',
+            'serializers': [{
+                name: 'def',
+                fn: function(rooms) {
+                    return '<input type="hidden" name="myrooms" value="' + $.map(rooms, function(room) {
+                        return $.map(room, function(pax) { return pax; }).join('');
+                    }).join(";") + '" />';
+                } }]
             };
 
-            $this.data(data);
+            if (options) {
+                $.extend(settings, options);
+            }
 
-            $(this).change(function() {
+            function update_rooms(rooms_data, upTo) {
 
-                var current_data = $(this).data();
-                current_data.rooms = setRooms(current_data.rooms, $(this).val());
-                $(this).data(current_data);
-                $("#" + current_data.form_id).html(formhtml(current_data.rooms));
+                var ret = [],
+                currentRooms = extract_rooms(rooms_data.form_id);
+
+                for (var i = 0; i < upTo; i++) {
+                    ret.push(currentRooms[i] || settings.default_room);
+                }
+                console.log(ret);
+                return ret;
+            }
+
+            function extract_rooms(form_id) {
+                var ret = [];
+                $("#" + form_id + " fieldset").each(function() {
+
+                    var nRoom = [];
+
+                    $(this).find("select[name='child-pax']").each(function() {
+                        nRoom.push($(this).val());
+                    });
+
+                    $(this).find("select[name='adult-pax']").each(function() {
+                        var amount = $(this).val();
+
+                        $.each(new Array(amount - 0), function() {
+                            nRoom.push("a");
+                        });
+                    });
+
+                    ret.push(nRoom);
+
+                });
+
+                return ret;
+            }
+
+
+            function htmlTag(tag, content, attributes) {
+                var serialized_attributes = '';
+
+                for (var n in attributes) {
+                    serialized_attributes += n + '="' + attributes[n] + '"';
+                }
+                if ((content !== undefined && content !== '')) {
+                    return "<" + tag + ' ' + serialized_attributes + ">" + content + "</" + tag + ">";
+                } else {
+                    return "<" + tag + ' ' + serialized_attributes + "/>";
+                }
+            }
+
+            function htmlSelect(name, start, end, selected) {
+                if (!selected) {
+                    selected = start;
+                }
+                var buffer = '';
+
+                for (; start <= end; start++) {
+                    if (selected == start) {
+                        buffer += htmlTag("option", start, { selected: 'selected' });
+                    } else {
+                        buffer += htmlTag("option", start);
+                    }
+                }
+
+                return htmlTag("select", buffer, { name: name });
+            }
+
+            function countAdults(room) {
+                return $.grep(room, function(val) { return val == "a" }).length;
+            }
+
+            function countChildren(room) {
+                return room.length - countAdults(room);
+            }
+
+            function extract_children(room) {
+                return $.grep(room, function(val) { return val !== "a" });
+            }
+
+            function roomSerializer(room) {
+                var allChildren = extract_children(room);
+
+                var adults = htmlTag("label", settings.adults_label + htmlSelect("adult-pax", settings.min_adults, settings.max_adults, countAdults(room)));
+                var childrenHTML = htmlTag("label", settings.children_label + htmlSelect("children", settings.min_children, settings.max_children, allChildren.length));
+                
+                return adults +
+                        childrenHTML +
+                        "<ul>" +
+
+                        $.map(allChildren, function(a) {
+                            return htmlTag("li", htmlSelect("child-pax", settings.min_child_age, settings.max_child_age, a));
+                        }).join("") +
+                        "</ul>";
+            }
+
+            function formhtml(rooms) {
+                var htmlRooms = $.map(rooms, function(room) {
+                    return htmlTag("fieldset", roomSerializer(room));
+                }).join('');
+                var serializedRooms = $.map(settings.serializers, function(serializer) {
+                    return serializer.fn(rooms);
+                }).join("");
+                return htmlRooms + serializedRooms;
+            };
+
+            function getRandomNumber(range) {
+                return Math.floor(Math.random() * range);
+            }
+
+            function getRandomChar() {
+                var chars = "0123456789abcdefghijklmnopqurstuvwxyz";
+                return chars.substr(getRandomNumber(39), 1);
+            }
+
+            function randomID(size) {
+                var str = "";
+                for (var i = 0; i < size; i++) {
+                    str += getRandomChar();
+                }
+                return str;
+            }
+
+            return this.each(function(i, el) {
+                var $this = $(this), data = {
+                    rooms: [settings.default_room],
+                    form_id: "pngr_rooms_" + randomID(10)
+                };
+
+                $this.data(data);
+
+                $(this).change(function() {
+
+                    var current_data = $(this).data();
+                    current_data.rooms = update_rooms(current_data, $(this).val());
+                    $(this).data(current_data);
+                    console.log("current data");
+                    console.log(current_data);
+                    $("#" + current_data.form_id).html(formhtml(current_data.rooms));
+
+                });
+
+                $this.after('<div id="' + data.form_id + '">' + formhtml(data.rooms) + '</div>');
+
+                $("#" + data.form_id + " fieldset select[name='children']").live("change", function() {
+
+                    var currentAges = [], newAges = [], desired_childrens = $(this).val();
+
+                    $(this).parents("fieldset").find("select[name='child-pax']").each(function() {
+                        currentAges.push($(this).val());
+                    });
+                    
+                    for (var i = 0; i < desired_childrens; i++) {
+                        newAges.push(currentAges[i] || settings.min_child_age);
+                    }
+
+                    var theList = $(this).parents("fieldset").find("ul");
+                    theList.children().remove();
+                    theList.html($.map(newAges, function(val) {
+                        return htmlTag("li", htmlSelect("child-pax", settings.min_child_age, settings.max_child_age, val || settings.min_child_age));
+                    }).join(""));
+                });
 
             });
-
-            $this.after('<div id="' + data.form_id + '">' + formhtml(data.rooms) + '</div>');
-
-        });
-    };
-})(jQuery);
+        };
+    })(jQuery);
